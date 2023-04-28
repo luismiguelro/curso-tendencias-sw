d4ec348f53b39943ecee0c5a6791851733078d33
Luis Miguel Rodriguez <luismiguelrodriguez33@gmail.com>
2023-04-28 11:29:09 -0500

    Clase 28/04/2023: Conexión a PostgresSQL

--- clases_y_ejercicios/Django/personas_django/sap/sap/settings.py
+++ clases_y_ejercicios/Django/personas_django/sap/sap/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'webapp',
+    'personas',
 ]
 
 MIDDLEWARE = [
@@ -74,10 +75,15 @@ WSGI_APPLICATION = 'sap.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'sap_db',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '5433',
     }
 }
 
--- clases_y_ejercicios/Django/personas_django/sap/webapp/views.py
+++ clases_y_ejercicios/Django/personas_django/sap/webapp/views.py
@@ -1,10 +1,14 @@
 from django.http import  HttpResponse
 from django.shortcuts import render
-
+from personas.models import Persona
 # Create your views here.
+
 def bienvenido(request):
+    num_personas= Persona.objects.count()
+    personas = Persona.objects.all()
+    DicMensajes={'msg1':'Valor mensaje 1'}
     # Renderizar html en django
-    return render(request,'EstudiantesTdeA.html')
+    return render(request,'EstudiantesTdeA.html',{'no_personas':num_personas,'personas':personas})
 
 def despedirse(request):
     return HttpResponse('Despedida desde Django')
